93ab13ea3a44368c1bf612be0179ef475421afde
micady <kotaro.a.tachibana@dena.com>
2023-07-06 14:10:22 +0900

    [update]add trpg_system enum

--- lib/model/scenario/scenario.dart
+++ lib/model/scenario/scenario.dart
@@ -1,11 +1,15 @@
+import 'dart:ui';
+
 import 'package:freezed_annotation/freezed_annotation.dart';
 
+part 'trpg_system.dart';
 part 'scenario.freezed.dart';
 
 @freezed
 class Scenario with _$Scenario {
   const factory Scenario({
     required int id,
+    required TRPGSystem system,
     required String name,
     String? url,
     String? image,
