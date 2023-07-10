22728707d360677de20d16b90373a0c4fa148f6b
micady <kotaro.a.tachibana@dena.com>
2023-07-10 22:53:33 +0900

    [update]中途半端だけどViewModel作り始めた

---
+++ lib/view_model/universal/user/user_view_model.dart
@@ -0,0 +1,31 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+import 'package:scenarioshelf/repository/remote/firebase/auth/firebase_auth_remote_repository.dart';
+
+part 'user_view_model.g.dart';
+
+@Riverpod(keepAlive: true)
+class UserViewModel extends _$UserViewModel {
+  late final FirebaseAuthAPI _authRepository;
+
+  @override
+  FutureOr<User?> build() {
+    _authRepository = ref.watch(firebaseAuthRemoteRepositoryProvider);
+
+    return _authRepository.getCurrentUser();
+  }
+
+  Future<void> signupWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    state = const AsyncValue.loading();
+    // TODO: validation
+    state = await AsyncValue.guard(
+      () => _authRepository.signupWithEmailAndPassword(
+        email: email,
+        password: password,
+      ),
+    );
+  }
+}
---
+++ lib/view_model/universal/user/user_view_model.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$userViewModelHash() => r'e798aa0a5d598a843f3c81f019904aa3f639b887';
+
+/// See also [UserViewModel].
+@ProviderFor(UserViewModel)
+final userViewModelProvider =
+    AsyncNotifierProvider<UserViewModel, User?>.internal(
+  UserViewModel.new,
+  name: r'userViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$userViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$UserViewModel = AsyncNotifier<User?>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
