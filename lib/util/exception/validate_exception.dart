91e3e3de798a7645467ac70baa5f063d3f95a263
micady <kotaro.a.tachibana@dena.com>
2023-07-11 23:10:34 +0900

    [delete]イメージつかないから巻き戻してViewから作る

--- lib/util/exception/validate_exception.dart
+++
@@ -1,8 +0,0 @@
-class ValidateException implements Exception {
-  const ValidateException(this.message);
-
-  final String message;
-
-  @override
-  String toString() => 'ValidateException: $message';
-}
--- lib/view_model/universal/user/user_view_model.dart
+++ lib/view_model/universal/user/user_view_model.dart
@@ -1,8 +1,6 @@
-import 'package:email_validator/email_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:scenarioshelf/repository/remote/firebase/auth/firebase_auth_remote_repository.dart';
-import 'package:scenarioshelf/util/exception/validate_exception.dart';
 
 part 'user_view_model.g.dart';
 
@@ -22,9 +20,6 @@ class UserViewModel extends _$UserViewModel {
     required String password,
   }) async {
     state = const AsyncValue.loading();
-
-    if (!EmailValidator.validate(email)) throw const ValidateException('failed to validate from email');
-
     state = await AsyncValue.guard(
       () => _authRepository.signupWithEmailAndPassword(
         email: email,
