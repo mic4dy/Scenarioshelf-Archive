ef15aa519f7d2d9986f4afcf830d246a2704c2ae
micady <kotaro.a.tachibana@dena.com>
2023-07-16 12:20:44 +0900

    [update]fix view

--- lib/view/signup/signup_page.dart
+++ lib/view/signup/signup_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:scenarioshelf/constant/app_color.dart';
 import 'package:scenarioshelf/constant/app_size.dart';
+import 'package:scenarioshelf/router/router.dart';
 import 'package:scenarioshelf/state/signing/signing_state.dart';
 import 'package:scenarioshelf/view/boot/boot_option_button.dart';
 import 'package:scenarioshelf/view/component/signing_form.dart';
@@ -15,38 +17,55 @@ class SignupPage extends ConsumerWidget {
     final SigningState state = ref.watch(signingViewModelProvider);
 
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(
-          left: size.width * 0.1,
-          right: size.width * 0.1,
-        ),
-        child: Column(
+      body: SafeArea(
+        child: Stack(
           children: [
-            const Spacer(flex: 3),
-            Image.asset(
-              'assets/images/logo/logo.png',
-              width: size.width * 0.4,
-            ),
-            const Spacer(flex: 2),
-            SigningForm.email(),
-            const SizedBox(height: MarginSize.large),
-            SigningForm.password(
-              isObscure: state.isObscure,
-              suffixIcon: IconButton(
-                onPressed: () => ref.read(signingViewModelProvider.notifier).onTapObscureIcon(),
-                icon: Icon(state.isObscure ? Icons.visibility_off_outlined : Icons.visibility_outlined),
-                iconSize: 18,
-                splashRadius: 20,
+            Align(
+              alignment: Alignment.topRight,
+              child: Container(
+                margin: const EdgeInsets.all(MarginSize.small),
+                child: FloatingActionButton.small(
+                  onPressed: () => ref.read(routerProvider).pop(),
+                  backgroundColor: AppColor.brand.primary,
+                  elevation: 0,
+                  child: const Icon(Icons.clear),
+                ),
               ),
             ),
-            const Spacer(flex: 2),
-            BootOptionButton.dark(
-              onPressed: () {},
-              label: '登録',
-              textLetterSpace: SpaceSize.bootOptionButtonLetterSpace,
-              size: Size(size.width * 0.8, 40),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: size.width * 0.1,
+              ),
+              child: Column(
+                children: [
+                  const Spacer(flex: 2),
+                  Image.asset(
+                    'assets/images/logo/logo.png',
+                    width: size.width * 0.4,
+                  ),
+                  const Spacer(flex: 2),
+                  SigningForm.email(),
+                  const SizedBox(height: MarginSize.large),
+                  SigningForm.password(
+                    isObscure: state.isObscure,
+                    suffixIcon: IconButton(
+                      onPressed: () => ref.read(signingViewModelProvider.notifier).onTapObscureIcon(),
+                      icon: Icon(state.isObscure ? Icons.visibility_off_outlined : Icons.visibility_outlined),
+                      iconSize: 18,
+                      splashRadius: 20,
+                    ),
+                  ),
+                  const Spacer(flex: 2),
+                  BootOptionButton.dark(
+                    onPressed: () {},
+                    label: '登録',
+                    textLetterSpace: SpaceSize.bootOptionButtonLetterSpace,
+                    size: Size(size.width * 0.8, 40),
+                  ),
+                  const Spacer(),
+                ],
+              ),
             ),
-            const Spacer(),
           ],
         ),
       ),
