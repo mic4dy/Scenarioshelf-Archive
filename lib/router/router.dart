8d54cb9f8a016e71984178d0dbe515a292eb953a
micady <kotaro.a.tachibana@dena.com>
2023-07-18 22:05:29 +0900

    [update]add routing

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -4,6 +4,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:scenarioshelf/view/boot/boot_page.dart';
 import 'package:scenarioshelf/view/signing/signin/signin_page.dart';
 import 'package:scenarioshelf/view/signing/signup/signup_page.dart';
+import 'package:scenarioshelf/view_model/universal/user/user_view_model.dart';
 
 part 'router.g.dart';
 part 'routes.dart';
@@ -15,7 +16,13 @@ GoRouter router(RouterRef ref) {
     routes: [
       GoRoute(
         path: Routes.splash.path,
-        redirect: (context, state) => Routes.boot.path,
+        redirect: (context, state) {
+          final userState = ref.watch(userViewModelProvider);
+
+          if (userState is AsyncData && userState.value != null) return Routes.home.path;
+
+          return Routes.boot.path;
+        },
       ),
       GoRoute(
         path: Routes.boot.path,
